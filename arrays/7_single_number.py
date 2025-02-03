f6505eae6d9d05fd4e684cdc6d720a9fcd8c3b96
Aftaab Siddiqui <aftaab2507@gmail.com>
2025-02-04 02:17:06 +0530

    add array question solutions

---
+++ arrays/7_single_number.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        x = nums[0]
+        for i in range(1, len(nums)):
+            x ^= nums[i]
+
+        return x
