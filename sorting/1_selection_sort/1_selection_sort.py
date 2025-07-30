a508b0aa3483b0331d8ec9fd08bf23b3697fc90e
maskedsyntax <aftaab2507@gmail.com>
2025-07-30 18:21:08 +0530

    fresh start

--- sorting/1_selection_sort/1_selection_sort.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def selectionSort(self, arr):
-        for i in range(0, len(arr)):
-            min = i
-            for j in range(i + 1, len(arr)):
-                if arr[j] < arr[min]:
-                    min = j
-
-            arr[i], arr[min] = arr[min], arr[i]
